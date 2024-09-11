3a4d9c3127caca47b6afdeff6bd73f66e7475d38
mamecurry <roddyroddyroddyroddy@gmail.com>
2024-09-11 16:23:11 +0900

    一覧機能作成

--- app/Http/Controllers/MemoController.php
+++ app/Http/Controllers/MemoController.php
@@ -8,6 +8,10 @@ use Illuminate\Http\Request;
 
 class MemoController extends Controller
 {
+    public function index(){
+    $memos=Memo::all();
+    return view('memos.index',['memos'=>$memos]);
+    }
     public function show($id)
     {
 
--- resources/views/memos/index.blade.php
+++ resources/views/memos/index.blade.php
@@ -4,11 +4,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>memo show</title>
+    <title>index</title>
 </head>
 <body>
-<a href="/memos"<a href="{{ route('memos.index') }}">戻る</a>
-    <h1>{{ $memo->title }}</h1>
-    <p>{!! nl2br(e($memo->body)) !!}</p>
+    <h1>タイトル一覧</h1>
+    <ul>
+    @foreach ($memos as $memo)
+        <li><a href="{{ route('memos.show',$memo) }}">{{ $memo->title }}</li>
+        
+    @endforeach
+        </ul>
 </body>
 </html>
