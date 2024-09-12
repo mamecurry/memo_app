2f731254f549725b1428a9997c9cef4376f7d7e7
mamecurry <roddyroddyroddyroddy@gmail.com>
2024-09-12 10:38:42 +0900

    削除機能作成

--- app/Http/Controllers/MemoController.php
+++ app/Http/Controllers/MemoController.php
@@ -56,4 +56,11 @@ class MemoController extends Controller
         // 登録したらindexに戻る
         return redirect('/memos');
     }
+    public function destroy($id)
+    {
+        $memo = Memo::find($id);
+        $memo->delete();
+
+        return redirect('/memos');
+    }
 }
